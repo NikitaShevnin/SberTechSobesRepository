1847a1243dcf59b78336d4898f369377ef0e8912
NikitaShevnin <117452749+NikitaShevnin@users.noreply.github.com>
2025-06-10 21:44:41 +0300

    Add AOP support and import aspect in tests

--- src/test/java/ru/sberTechSobes/parkingProject/controller/ParkingControllerTest.java
+++ src/test/java/ru/sberTechSobes/parkingProject/controller/ParkingControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.sberTechSobes.parkingProject.enumeration.VehicleType;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ParkingController.class)
+@Import(ru.sberTechSobes.parkingProject.aop.ServiceExceptionAspect.class)
 class ParkingControllerTest {
 
     @Autowired
